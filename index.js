9eaefb72fccfaf22a8fb1db82b74e6e3cbf77c2e
EvaKhekalo <eva.khekalo@gmail.com>
2026-06-09 13:16:45 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let button1 = document.querySelector(".btn");
+let button2 = document.querySelector(".btn3_1");
+
+button1.addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+button2.addEventListener("click", function() {
+    window.location.href = "login.html";
+});
